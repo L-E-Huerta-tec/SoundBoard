0a6dc40c67bede2d3dcceb55919392bf4f6f3875
L-E-Huerta-tec <luis.huerta@tecsup.edu.pe>
2024-10-07 17:03:53 -0500

    Initial Commit

---
+++ HuertaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuertaSoundBoard
+//
+//  Created by Luia Enrique Huerta Gonzales on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
